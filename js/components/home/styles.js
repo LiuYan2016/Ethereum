a67d47888742ad17c55b3ed1c81c379ec62d15d7
LiuYan <Nicky3734@yandex.com>
2017-09-18 12:37:40 +0100

    first commit

---
+++ js/components/home/styles.js
@@ -0,0 +1,33 @@
+
+const React = require('react-native');
+
+const { StyleSheet } = React;
+export default {
+  container: {
+    backgroundColor: '#FBFAFA',
+  },
+  headerStyle:{
+    backgroundColor: '#f5f5f5',
+    alignItems : 'stretch',
+   },
+  headerColor:{
+    color:'#1f1f1f',
+    alignSelf:'center',
+    marginLeft:60
+  },
+  hColor:{
+    color:'#1f1f1f'
+  },
+  row: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    marginBottom: 15,
+    alignItems: 'center',
+  },
+  mt: {
+    marginTop: 18,
+  }
+};
---
+++ js/components/overviewComponent/styles.js
@@ -0,0 +1,36 @@
+const React = require('react-native');
+
+const { StyleSheet } = React;
+export default {
+    mt20 : {
+        marginTop : 20
+    },
+    topText : {
+        marginTop: 20,
+        marginBottom : 20,
+        textAlign: 'center',
+        fontSize: 50,
+        color:'#aaa'
+    },
+    red : {
+        color: '#f00'
+    },
+    green : {
+        color : '#0f0'
+    },
+    pickerBGStyle : {
+        backgroundColor:'#616161'
+    },
+    pickerColorStyle : {
+        color:'#fff'
+    },
+    redSmallText : {
+        fontSize : 10,
+        color: '#f00'
+    },
+    greenSmallText : {
+        fontSize : 10,
+        color : '#0f0'
+    }
+
+};
